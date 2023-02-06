e2c76a251753c8f92b90376dee3fa8288be83429
Duong <duong.dv@shopstack.asia>
2023-02-06 13:41:19 +0700

    update print

---
+++ app/code/WP/PrintProduct/Block/PrintProduct.php
@@ -0,0 +1,9 @@
+<?php
+namespace WP\PrintProduct\Block;
+class PrintProduct extends \Magento\Framework\View\Element\Template
+{
+    public function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+}
---
+++ app/code/WP/PrintProduct/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+	'WP_PrintProduct',
+	__DIR__
+);
